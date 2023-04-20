1fb445b77c0cb5f865a978c9cc0aa9ef29b58ec7
Rameshwar <rameshwar.yadav@capgemini.com>
2023-04-20 10:51:35 +0530

    Book Api Integration

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,24 +1,28 @@
-import { Component } from '@angular/core';
+
+
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BookServiceService } from '../services/BookServices/book-service.service';
 
-// For table
-export interface book {
-  name: string;
-  author: string;
-  price: number;
-  course: string;
-  action: number;
+
+
+
+export interface Status {
+  value: boolean;
+  viewValue: string;
 }
-
-const bookDetails: book[] = [
-  {name: 'Hydrogen', author: 'Hydra', price: 100, course: 'BTech', action:1},
-  {name: 'Hydrogen', author: 'Hydrogen', price: 250, course: 'BTech', action:1},
-];
-
-// For dropdown
-interface Course {
+export interface Course {
   value: string;
   viewValue: string;
+}
+
+
+export interface book {
+  // bookId:any;
+  bookName: any;
+  bookAuthor: any;
+  bookPrice:any;
+  course: any;
 }
 
 @Component({
@@ -28,56 +32,174 @@ interface Course {
 })
 
 export class BookComponent {
-  // For Form
-  bookForm = new FormGroup({
-    bName: new FormControl('', Validators.required),
-    bAuthor: new FormControl('', Validators.required),
-    price: new FormControl('', [Validators.required, Validators.min(1)]),
+  constructor(private bookService : BookServiceService) { }
+
+ 
+  title: any;
+  hideControl: any;
+  formName: any = true;
+
+  BookList: any = [];
+  showBookList() {
+    this.bookService.showAllBooks().subscribe(data => {
+      this.BookList = data;
+      console.log(data);
+    });
+  }
+
+  getBookById(id: number) {
+    this.hideControl = true;
+    this.formName = false;
+    this.bookService.getBookById(id).subscribe(data => {
+      this.openModel();
+      this.title = "Update Book"
+      this.updateForm = new FormGroup({
+        bookId: new FormControl(data.bookId, Validators.required),
+        bookName: new FormControl(data.bookName, Validators.required),
+        bookAuthor: new FormControl(data.bookAuthor, Validators.required),
+        bookPrice: new FormControl(data.bookPrice,  [Validators.required,Validators.min(1)]),
+        course: new FormControl(data.course, Validators.required),
+        isDeleted: new FormControl(data.isDeleted, Validators.required)
+      });
+      console.log(data);
+    });
+  }
+
+  AddUpdateBook() {
+    if (this.title == "Add Book") {
+      this.hideControl = false;
+      this.bookService.addBook(this.addForm.value)
+        .subscribe({
+          next: (response) => {
+            this.CloseModel();
+            window.alert("Book added SuccessFully");
+            location.reload();
+          },
+          error: (error: any) => {
+            window.alert(error.error);
+            console.log(error);
+          }
+        });
+    }
+    else {
+      this.formName = false;
+      this.bookService.updateBook(this.updateForm.value)
+        .subscribe({
+          next: (response) => {
+            this.CloseModel();
+            window.alert("Book updated SuccessFully");
+            location.reload();
+          },
+          error: (error: any) => {
+            window.alert(error.error);
+            console.log(error);
+          }
+        });
+    }
+  }
+
+  deleteBook(id: number): void {
+    if (window.confirm("Are you sure you want to delete this book ?")) {
+      this.bookService.deleteBook(id).subscribe({
+        next: (response: ArrayBuffer) => {
+          const message = new TextDecoder().decode(response);
+          window.alert(message);
+          console.log(message);
+          location.reload();
+        },
+        error: (error: any) => {
+          console.error(error);
+        }
+      });
+    }
+  }
+
+  panelOpenState1 = true;
+
+  updateForm = new FormGroup({
+    bookName: new FormControl('', Validators.required),
+    bookAuthor: new FormControl('', Validators.required),
+    bookId: new FormControl('', Validators.required),
+    bookPrice: new FormControl('',  [Validators.required, Validators.min(1)]),//,Validators.min(1)  Validators.pattern(/^\d+$/)
+    course: new FormControl('', Validators.required),
+    isDeleted: new FormControl('', Validators.required)
+  });
+
+  addForm = new FormGroup({
+    bookName: new FormControl('', Validators.required),
+    bookAuthor: new FormControl('', Validators.required),
+    bookPrice: new FormControl('',  [Validators.required, Validators.min(1)]),
     course: new FormControl('', Validators.required)
   });
 
-  bookSubmitted(){
-    console.log(this.bookForm.value);
+
+  bookSubmitted() {
+    console.log(this.updateForm.value);
   }
 
-  get bName(): FormControl {
-    return this.bookForm.get('bName') as FormControl;
-  }
-
-  get bAuthor(): FormControl {
-    return this.bookForm.get('bAuthor') as FormControl;
-  }
-
-  get price(): FormControl {
-    return this.bookForm.get('price') as FormControl;
-  }
-
-  get course(): FormControl {
-    return this.bookForm.get('course') as FormControl;
-  }
-
-  // For Modal
+  // For modal
   openModel() {
     const modelDiv = document.getElementById('myModal');
-    if(modelDiv!= null) {
+    if (modelDiv != null) {
       modelDiv.style.display = 'block';
-    } 
+    }
   }
 
   CloseModel() {
     const modelDiv = document.getElementById('myModal');
-    if(modelDiv!= null) {
+    if (modelDiv != null) {
       modelDiv.style.display = 'none';
-    } 
+      location.reload();
+    }
   }
 
-  courses: Course[] = [
-    {value: 'BTech', viewValue: 'BTech'},
-    {value: 'BCA', viewValue: 'BCA'},
-    {value: 'BA', viewValue: 'BA'},
+  ngOnInit(): void {
+    this.showBookList();
+    this.title = "Add Book"
+  }
+
+  get validName(): FormControl {
+    if (this.title == "Add Book") {
+      return this.addForm.get('bookName') as FormControl;
+    } else {
+      return this.updateForm.get('bookName') as FormControl;
+    }
+  }
+  get validAuthor(): FormControl {
+    if (this.title == "Add Book") {
+      return this.addForm.get('bookAuthor') as FormControl;
+    } else {
+      return this.updateForm.get('bookAuthor') as FormControl;
+    }
+  }
+
+  get validPrice(): FormControl {
+    if (this.title == "Add Book") {
+      return this.addForm.get('bookPrice') as FormControl;
+    } else {
+      return this.updateForm.get('bookPrice') as FormControl;
+    }
+  }
+
+  get validCourse(): FormControl {
+    if (this.title == "Add Book") {
+      return this.addForm.get('course') as FormControl;
+    } else {
+      return this.updateForm.get('course') as FormControl;
+    }
+  }
+
+  status: Status[] = [
+    { value: true, viewValue: 'Inctive' },
+    { value: false, viewValue: 'Active' },
   ];
 
-  // Table
-  displayedColumns: string[] = ['name', 'author', 'price', 'course', 'action'];
-  dataSource = bookDetails;
+  courses: Course[] = [
+        {value: 'B.tech', viewValue: 'B.tech'},
+        {value: 'BCA', viewValue: 'BCA'},
+        {value: 'BA', viewValue: 'BA'},
+      ];
+    
+  // For table
+  displayedColumns: string[] = ['id', 'bookName', 'bookAthor','bookPrice','course','action'];
 }
--- src/app/university/university.component.ts
+++ src/app/university/university.component.ts
@@ -1,99 +1,27 @@
-// import { Component, OnInit, ViewChild } from '@angular/core';
-// import { FormControl, FormGroup, Validators } from '@angular/forms';
-
-// export interface university {
-//   id: number;
-//   name: string;
-//   address: string;
-//   action: number;
-// }
-
-// const universityDetails: university[] = [
-//   {id: 1001, name: 'Solicon', address:'Mumbai', action: 1},
-//   {id: 1002, name: 'DAVV', address: 'Indore', action: 2},
-//   {id: 1003, name: 'HIT', address: 'Haldia', action: 1}
-// ];
-
-// @Component({
-//   selector: 'app-university',
-//   templateUrl: './university.component.html',
-//   styleUrls: ['./university.component.css']
-// })
-
-// export class UniversityComponent{
-  
-//   panelOpenState1 = true;
-
-//   // For Forms
-//   universityForm = new FormGroup({
-//     uniName: new FormControl('', Validators.required),
-//     address: new FormControl('', Validators.required),
-//   });
-
-//   uniSubmitted(){
-//     console.log(this.universityForm.value);
-//   }
-
-//   get uniName(): FormControl {
-//     return this.universityForm.get('uniName') as FormControl;
-//   }
-
-//   get address(): FormControl {
-//     return this.universityForm.get('address') as FormControl;
-//   }
-//   // For modal
-//   openModel() {
-//     const modelDiv = document.getElementById('myModal');
-//     if(modelDiv!= null) {
-//       modelDiv.style.display = 'block';
-//     } 
-//   }
-
-//   CloseModel() {
-//     const modelDiv = document.getElementById('myModal');
-//     if(modelDiv!= null) {
-//       modelDiv.style.display = 'none';
-//     } 
-//   }
-
-//   // For table
-//   displayedColumns: string[] = ['id', 'name', 'address', 'action'];
-//   dataSource = universityDetails;
-
-// }
-
-
 
 import { Component, OnInit} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog'; // Import the MatDialogModule
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
 import { UniversitiyServiceService } from '../services/UniversityServices/universitiy-service.service';
-// import { UniversitiyServiceService } from '../services/UniversityServices/universitiy-service.service';
 
-declare var window:any;
-
-export interface PeriodicElement {
-  universityId: number;
-  name: string;
-  address: string;
+export interface Status {
+  value: boolean;
+  viewValue: string;
 }
 
+export interface University{
+  name: any;
+  address: any;
+}
 
 @Component({
   selector: 'app-university',
   templateUrl: './university.component.html',
   styleUrls: ['./university.component.css'],
-  providers: [] // Add the MatDialogModule to the providers array
 })
 
-export class UniversityComponent implements OnInit{
-
+export class UniversityComponent {
 constructor(private universityService:UniversitiyServiceService) {}
-Name: any;
-address: any;
+
 title: any;
 hideControl: any;
 formName: any = true;
@@ -103,29 +31,25 @@ formName: any = true;
   showUniversityList(){
     this.universityService.showAllUniversities().subscribe(data=>{
       this.UniversityList=data;
+      console.log(data);
     });
   }
 
-  // getUniversityById(id: number) {
-  //   this.hideControl = true;
-  //   this.formName = false;
-  //   this.universityService.getUniversityById(id).subscribe(data => {
-  //     this.openModel();
-  //     this.title = "Update University"
-  //     this.updateForm = new FormGroup({
-  //        universityId: new FormControl(data.universityId, Validators.required),
-  //       Name: new FormControl(data.Name, Validators.required),
-       
-  //       address: new FormControl(data.address, Validators.required),
-  //       // universityId: new FormControl(data.universityId, Validators.required),
-  //       // term: new FormControl(data.term, Validators.required),
-  //       // course: new FormControl(data.course, Validators.required),
-  //       isDeleted: new FormControl(data.isDeleted, Validators.required)
-  //     });
-  //     console.log(data);
-  //   });
-  // }
-
+  getUniversityById(id: number) {
+    this.hideControl = true;
+    this.formName = false;
+    this.universityService.getUniversityById(id).subscribe(data => {
+      this.openModel();
+      this.title = "Update University"
+      this.updateForm = new FormGroup({
+         universityId: new FormControl(data.universityId, Validators.required),
+        name: new FormControl(data.name, Validators.required),
+        address: new FormControl(data.address, Validators.required),
+        isDeleted: new FormControl(data.isDeleted, Validators.required)
+      });
+      console.log(data);
+    });
+  }
 
   AddUpdateUniversity() {
     if (this.title == "Add University") {
@@ -138,12 +62,7 @@ formName: any = true;
             location.reload();
           },
           error: (error: any) => {
-            if (this.addForm.value.address == null || this.addForm.value.Name == null ) {
-              window.alert("The University data provided appears to be invalid or null")
-            }
-            else {
-              window.alert(error.error);
-            }
+            window.alert(error.error);
             console.log(error);
           }
         });
@@ -158,20 +77,15 @@ formName: any = true;
             location.reload();
           },
           error: (error: any) => {
-            if (this.address == null ||  this.Name == null ) {
-              window.alert("Please fill all details")
-            }
-            else {
-              window.alert(error.error);
-            }
+            window.alert(error.error);
             console.log(error);
           }
         });
     }
   }
 
-
   deleteUniversity(id: number): void {
+    if (window.confirm("Are you sure you want to delete this university ?")) {
     this.universityService.deleteUniversity(id).subscribe({
       next: (response: ArrayBuffer) => {
         const message = new TextDecoder().decode(response);
@@ -184,31 +98,28 @@ formName: any = true;
       }
     });
   }
+  }
+
   panelOpenState1 = true;
+
   updateForm = new FormGroup({
-    // studentId: new FormControl('', Validators.required),
-    Name: new FormControl('', Validators.required),
-    // email: new FormControl('', [Validators.required, Validators.email]),
+    name: new FormControl('', Validators.required),
     address: new FormControl('', Validators.required),
     universityId: new FormControl('', Validators.required),
-    // term: new FormControl('', Validators.required),
-    // course: new FormControl('', Validators.required),
     isDeleted: new FormControl('', Validators.required)
   });
 
   addForm = new FormGroup({
-    Name: new FormControl('', Validators.required),
-    address: new FormControl('', Validators.required),
-    // universityId: new FormControl('', Validators.required),
-   
+    name: new FormControl('', Validators.required),
+    address: new FormControl('', Validators.required)
   });
   
  
   uniSubmitted() {
     console.log(this.updateForm.value);
   }
-  // For modal
 
+  // For modal
   openModel() {
     const modelDiv = document.getElementById('myModal');
     if (modelDiv != null) {
@@ -223,18 +134,32 @@ formName: any = true;
       location.reload();
     }
   }
-  
 
-  // ngOnInit(): void {
-  //   this.showUniversityList();
-  // }
   ngOnInit(): void {
     this.showUniversityList();
     this.title = "Add University"
   }
-  // displayedColumns: string[] = ['stuId', 'name', 'address', 'email', 'course', 'term', 'isDeleted', 'action'];
 
+  get validName(): FormControl {
+    if (this.title == "Add University") {
+      return this.addForm.get('name') as FormControl;
+    } else {
+      return this.updateForm.get('name') as FormControl;
+    }
+  }
+  get validAddress(): FormControl {
+    if (this.title == "Add University") {
+      return this.addForm.get('address') as FormControl;
+    } else {
+      return this.updateForm.get('address') as FormControl;
+    }
+  }
 
-  // For table
-  displayedColumns: string[] = ['id', 'Name', 'address', 'action'];
+  status: Status[] = [
+    { value: true, viewValue: 'Inctive' },
+    { value: false, viewValue: 'Active' },
+  ];
+
+   // For table
+  displayedColumns: string[] = ['id', 'name', 'address', 'action'];
 }
